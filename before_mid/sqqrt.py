37895c31ca1ef5d877526b05c2e0029cbd996128
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2024-09-25 22:14:34 +0600

    Create sqqrt.py

---
+++ before_mid/sqqrt.py
@@ -0,0 +1,14 @@
+import math
+
+a = int(input())
+b = math.sqrt(a)
+
+print(f"the root of {a} is = {b}")
+
+def sqqqqrt(a) :
+    if a<0 :
+        print("this is not root")
+    else :
+        print("yessss")
+
+root_num = sqqqqrt(a)
