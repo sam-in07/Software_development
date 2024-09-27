0f33ae488433b078fceb88546d8886dd3bf6400b
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2024-09-28 05:58:40 +0600

    Update and rename gcd.py to gcdlcm.py

--- basic_2/gcdlcm.py
+++ basic_2/gcdlcm.py
@@ -2,9 +2,10 @@
 def find_gcd(a,b) :
     while b :
         a , b = b , a%b
-    return a 
+    return a
 
-
+def find_lcm(a,b) :
+    return (a*b)//find_gcd(a,b)
 
 a  = int(input())
 b = int(input())
@@ -12,5 +13,6 @@ c  = int(input())
 
 
 lol = find_gcd(a,b)
-
+lol1 = find_lcm(a,b)
 print(f"gcd korbeeee yeaaaa {lol}")
+print(f"lcm korbeeee yeaaaa {lol1}")
