4871ecff6c4571566fceef8a3e1679e39d199fe9
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2024-09-28 09:06:55 +0600

    Update sum.py

--- basic_2/sum.py
+++ basic_2/sum.py
@@ -8,6 +8,7 @@ import random
 #sum of n
 
 
+'''
 def sum_of_n(n) :
     b = (n*(n+1))//2
     return  b
@@ -29,5 +30,23 @@ if n > 0 :
     print(f"sum of n numbersss  is {ans}")
 else :
     print(f"NO answer ")
+'''
+
+#solving by using a range
 
 
+
+def sum_of_n(start , n ):
+    sum = 0
+    for i in range(start,n+1):
+        sum += i
+
+    return  sum
+start = int(input())
+n = int(input())
+
+if start > 0 and  start <= n :
+    ans = sum_of_n(start , n )
+    print(f"sum of n numbersss  is {ans}")
+else :
+    print(f"NO answer ")
