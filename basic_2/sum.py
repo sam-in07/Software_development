1f9112cb60e3125b602b7025b0401c3a50c5722e
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2024-09-28 09:01:50 +0600

    Create sum.py

---
+++ basic_2/sum.py
@@ -0,0 +1,33 @@
+'''
+writer : sam__in07
+time : 8:31 am
+
+'''
+
+import random
+#sum of n
+
+
+def sum_of_n(n) :
+    b = (n*(n+1))//2
+    return  b
+
+
+
+
+
+
+
+
+
+
+n = float(input())
+#b = float(input())
+
+if n > 0 :
+    ans = sum_of_n(n)
+    print(f"sum of n numbersss  is {ans}")
+else :
+    print(f"NO answer ")
+
+
