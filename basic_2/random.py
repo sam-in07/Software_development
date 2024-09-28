af61cbb4dd8e295784b8f7da601e218bec62ad60
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2024-09-28 08:54:45 +0600

    Create random.py

---
+++ basic_2/random.py
@@ -0,0 +1,32 @@
+'''
+writer : sam__in07
+time : 8:31 am
+
+'''
+
+import random
+#code for random number
+
+
+def rand_fun(x,y) :
+    return random.uniform(x,y)
+
+
+
+
+
+
+
+
+
+
+a = float(input())
+b = float(input())
+
+
+my_random = rand_fun(a,b)
+
+print(f"print a random number between {a } and {b} is {my_random}")
+
+
+
