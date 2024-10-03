f56eb97ef90aeb08ac0277b6c56e0a793e6d75b4
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2024-10-03 23:54:23 +0600

    Create srt.py

---
+++ pert_3/srt.py
@@ -0,0 +1,12 @@
+'''
+writer : sam__in07
+time : 9:09 am
+
+'''
+
+#find second largest element
+
+numbers_list = list(map(int, input("Enter numbers separated by spaces: ").split()))
+numbers_list.sort()
+print(f"The ordered number list is = {numbers_list}")
+
