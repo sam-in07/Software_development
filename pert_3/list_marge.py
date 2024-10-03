71d3e1d8effd346ec64ee3c5b1bea53072f90864
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2024-10-03 23:59:56 +0600

    Create list_marge.py

---
+++ pert_3/list_marge.py
@@ -0,0 +1,4 @@
+list1 = list(map(int, input("Enter numbers for the first list, separated by spaces: ").split()))
+list2 = list(map(int, input("Enter numbers for the second list, separated by spaces: ").split()))
+merged_list = list1 + list2
+print(f"The merged list = {merged_list}")
