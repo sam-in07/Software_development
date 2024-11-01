fd8d8c2eeffd2a5933c424778f4fbee7ae7b6de7
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2024-11-01 20:54:25 +0600

    Create marged.py

---
+++ midlast/marged.py
@@ -0,0 +1,12 @@
+#  mayrge  a dectionary
+
+dictonary1 = {'a':1,'b':2,'c':2}
+dictonary2 ={'d':4,'e':5 , 'f':6}
+
+
+# marged_dictiononary = dictonary1.copy()
+# marged_dictiononary.update(dictonary2)
+dictonary1.update(dictonary2)
+
+# print(f"Mereged Dictonary :" , marged_dictiononary)
+print(f"Mereged Dictonary :" , dictonary1)
