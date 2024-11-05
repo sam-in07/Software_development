db25beb2261eb0450060878903fbbe57d06bea1c
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2024-11-05 13:58:28 +0600

    Update midlast

---
+++ midlast/lamda.py
@@ -0,0 +1,9 @@
+# Lambda function to add two numbers
+add_numbers = lambda x, y: x + y
+
+# Example usage
+num1 = 5
+num2 = 10
+result = add_numbers(num1, num2)
+
+print(f"The sum of {num1} and {num2} is {result}.")
